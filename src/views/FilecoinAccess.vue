4937e0d5ed43c7b50ef510bee3b573cc73fd6a48
Ian Darrow <ian.j.darrow@gmail.com>
2018-10-27 21:07:12 -0400

    fix invite layout, against all odds

--- src/views/FilecoinAccess.vue
+++ src/views/FilecoinAccess.vue
@@ -1,55 +1,58 @@
 <template>
   <!-- The page displayed to users who receive a filecoin invite -->
-  <div class="invite-container" :key="$route.params.token">
-    <img class="logo-top" src="../assets/invites/filecoin-logo-color.svg" />
-    <img class="illustration-top" src="../assets/invites/invite-tos-top.png" />
-    <div class="tos">
-      <h1>Welcome to the</h1>
-      <h1>Filecoin private preview!</h1>
-      <hr />
-      <p>Thanks for joining us as we work towards the public testnet and launch. We asked you to join because we value your deep expertise and thoughtful approach across a number of problem areas. Above all, we are grateful for your help, critique, PRs, ideas, and questions.</p>
-      <div class="rules">
-        <img class="side-image" src="../assets/invites/invite-tos-side.png" />
-        <div class="content">
-          <h1 ref="rules">Private preview rules</h1>
-          <span class="muted subtext">Check to agree</span>
-          <hr />
-          <ul>
-            <li>
-              <input type="checkbox" id="check1" v-model="checks[0]" class="styled-checkbox" />
-              <label for="check1">I agree not to share any code or information I receive as part of the private preview.</label>
-            </li>
-            <li>
-              <input type="checkbox" id="check2" v-model="checks[1]" class="styled-checkbox" />
-              <label for="check2">I agree not to use the Filecoin network to upload or share content that's illegal or violates the IP rights of others. I'll remove any such content if I'm able to do so.</label></li>
-            <li>
-              <input type="checkbox" id="check3" v-model="checks[2]" class="styled-checkbox" />
-              <label for="check3">I agree not to host a generally-accessible file upload service until the private preview ends (when the public testnet launches).</label></li>
-            <li>
-              <input type="checkbox" id="check4" v-model="checks[3]" class="styled-checkbox" />
-              <label for="check4">I agree not to blog, write, tweet, or otherwise publicly discuss the project or my participation until the public testnet launches.</label></li>
-            <li>
-              <input type="checkbox" id="check5" v-model="checks[4]" class="styled-checkbox" />
-              <label for="check5">I understand that I’m being granted access only for testing, collaboration, and feedback, and that there are no uptime or reliability guarantees.</label></li>
-          </ul>
-          <p>Your access may be revoked if you violate these rules. If you have any questions, email
-            <a href="mailto:legalrequests@protocol.ai">legalrequests@protocol.ai</a>, or feel free to reach out to another Protocol Labs contact.</p>
-        </div>
-      </div>
-      <div class="footer">
-        <h1>Get your access!</h1>
+  <div class="invite-container">
+    <div class="top-section">
+      <div class="left">
+        <img class="logo-top" src="../assets/invites/filecoin-logo-color.svg" />
+        <h1>Welcome to the<br />Filecoin private preview!</h1>
         <hr />
-        <form @submit.prevent="handleSubmit">
-          <div class="input mb-1 mt-1">
-            <label>Your GitHub username</label>
-            <input type="text" class="helper" v-model="form.github" ref="github">
-            <span class="helper">@</span>
-          </div>
-          <button type="submit" class="filecoin">
-            Agree and submit
-          </button>
-        </form>
+        <p>Thanks for joining us as we work towards the public testnet and launch. We asked you to join because we value your deep expertise and thoughtful approach across a number of problem areas. Above all, we are grateful for your help, critique, PRs, ideas, and questions.</p>
       </div>
+      <div class="right"><img class="illustration-top" src="../assets/invites/invite-tos-top.png" /></div>
+    </div>
+    <div class="bottom-section">
+      <div class="left">
+        <img class="side-image" src="../assets/invites/invite-tos-side.png" />
+      </div>
+      <div class="right">
+        <h1 ref="rules">Private preview rules</h1>
+        <span class="muted subtext">Check to agree</span>
+        <hr />
+        <ul>
+          <li>
+            <input type="checkbox" id="check1" v-model="checks[0]" class="styled-checkbox" />
+            <label for="check1">I agree not to share any code or information I receive as part of the private preview.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="check2" v-model="checks[1]" class="styled-checkbox" />
+            <label for="check2">I agree not to use the Filecoin network to upload or share content that's illegal or violates the IP rights of others. I'll remove any such content if I'm able to do so.</label></li>
+          <li>
+            <input type="checkbox" id="check3" v-model="checks[2]" class="styled-checkbox" />
+            <label for="check3">I agree not to host a generally-accessible file upload service until the private preview ends (when the public testnet launches).</label></li>
+          <li>
+            <input type="checkbox" id="check4" v-model="checks[3]" class="styled-checkbox" />
+            <label for="check4">I agree not to blog, write, tweet, or otherwise publicly discuss the project or my participation until the public testnet launches.</label></li>
+          <li>
+            <input type="checkbox" id="check5" v-model="checks[4]" class="styled-checkbox" />
+            <label for="check5">I understand that I’m being granted access only for testing, collaboration, and feedback, and that there are no uptime or reliability guarantees.</label></li>
+        </ul>
+        <p>Your access may be revoked if you violate these rules. If you have any questions, email
+          <a href="mailto:legalrequests@protocol.ai">legalrequests@protocol.ai</a>, or feel free to reach out to another Protocol Labs contact.</p>
+      </div>
+    </div>
+    <div class="footer">
+      <h1>Get your access!</h1>
+      <hr />
+      <form @submit.prevent="handleSubmit">
+        <div class="input mb-1 mt-1">
+          <label>Your GitHub username</label>
+          <input type="text" class="helper" v-model="form.github" ref="github">
+          <span class="helper">@</span>
+        </div>
+        <button type="submit" class="filecoin">
+          Agree and submit
+        </button>
+      </form>
     </div>
     <div class="bottom-border" />
   </div>
@@ -146,6 +149,121 @@ export default {
 
 <style lang="scss" scoped>
 @import "../styles/_colors";
+h1 {
+  font-size: 2.5rem;
+  @media (max-widthh: 640px) {
+    font-size: 2rem;
+  }
+}
+.invite-container {
+  padding: 1rem;
+  box-sizing: border-box;
+  overflow: hidden;
+  font-size: 120%;
+}
+.top-section {
+  display: flex;
+  position: relative;
+  min-height: 32rem;
+  @media (max-width: 640px) {
+    display: block;
+  }
+  .left {
+    padding-top: 100px;
+    flex-basis: 65%;
+    @media (min-width: 1000px) {
+      margin-left: calc(35vw - 350px);
+    }
+    @media (max-width: 700px) {
+      padding-top: 140px;
+    }
+    p {
+      max-width: 50ch;
+    }
+    .logo-top {
+      position: absolute;
+      width: 30%;
+      max-width: 320px;
+      top: 0;
+    }
+  }
+  .right {
+    img {
+      position: absolute;
+      top: -1rem;
+      right: -1rem;
+      width: 65%;
+      max-width: 650px;
+      pointer-events: none;
+    }
+  }
+}
+.bottom-section {
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+  h1 {
+    margin: 0;
+  }
+  span {
+    position: absolute;
+    transform: translateY(-0.25rem);
+    font-size: 80%;
+    font-style: italic;
+  }
+  @media (max-width: 700px) {
+    display: block;
+    padding-top: 60px;
+  }
+  ul {
+    list-style-type: none;
+    margin-left: 2rem;
+    li {
+      label:before {
+        position: absolute;
+        transform: translate(-2rem, 0.5rem);
+      }
+      label:after {
+        position: absolute;
+        transform: translate(-2rem, 0.3rem) rotate(45deg);
+      }
+    }
+  }
+  .left {
+    @media (max-width: 700px) {
+      display: none;
+    }
+    img {
+      position: absolute;
+      top: -3rem;
+      left: -1rem;
+      width: 45%;
+      max-width: 550px;
+      pointer-events: none;
+    }
+  }
+  .right {
+    flex-basis: 55%;
+    max-width: 45ch;
+    @media (min-width: 1000px) {
+      margin-right: calc(35vw - 350px);
+    }
+  }
+}
+.footer {
+  max-width: 800px;
+  margin: 0 auto;
+  padding-top: 100px;
+  padding-bottom: 160px;
+  text-align: center;
+  hr {
+    margin: 0 auto;
+    margin-bottom: 2rem;
+  }
+  button {
+    float: right;
+  }
+}
 hr {
   width: 80px;
   height: 3px;
@@ -153,137 +271,9 @@ hr {
   margin-top: 1.5rem;
   margin-bottom: 1.5rem;
 }
-.invite-container {
-  padding: 1rem;
-  padding-top: 320px;
-  max-width: 1200px;
-  margin: 0 auto;
-  a {
-    line-height: 1;
-  }
-  .illustration-top {
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 60%;
-  }
-  .logo-top {
-    position: absolute;
-    width: 24%;
-    top: 3rem;
-  }
-  @media (max-width: 600px) {
-    .illustration-top {
-      width: 100%;
-    }
-    .logo-top {
-      display: none;
-    }
-  }
-}
-.tos {
-  h1 {
-    font-weight: 500;
-    font-size: 3rem;
-    line-height: 1;
-    margin: 0;
-    margin-bottom: 0.5rem;
-    @media (max-width: 600px) {
-      font-size: 2rem;
-    }
-  }
-  p,
-  li {
-    font-size: 135%;
-    max-width: 55ch;
-    @media (max-width: 600px) {
-      font-size: 120%;
-    }
-  }
-}
-.rules {
-  padding-top: 100px;
-  padding-bottom: 100px;
-  display: flex;
-  justify-content: flex-end;
-  @media (max-width: 600px) {
-    padding-top: 60px;
-    padding-bottom: 60px;
-  }
-  .side-image {
-    position: absolute;
-    left: 0;
-    transform: translateY(-50px);
-    height: auto;
-    width: 50vw;
-    max-width: 600px;
-    @media (max-width: 1000px) {
-      transform: none;
-    }
-    @media (max-width: 800px) {
-      display: none;
-    }
-  }
-  .content {
-    width: 50vw;
-    padding-left: 1rem;
-    box-sizing: border-box;
-    li {
-      list-style-type: none;
-      position: relative;
-      label::before {
-        position: absolute;
-        transform: translate(-2rem, 0.55rem);
-      }
-      label::after {
-        position: absolute;
-        transform: translate(-2rem, 0.3rem) rotate(45deg);
-      }
-    }
-    h1 {
-      margin-bottom: 2rem;
-    }
-    span.subtext {
-      position: absolute;
-      transform: translateY(-2rem);
-      font-style: italic;
-    }
-    @media (max-width: 800px) {
-      width: auto;
-      padding-left: 0;
-    }
-  }
-}
-.footer {
-  padding-bottom: 150px;
-  max-width: 800px;
-  margin: 0 auto;
-  h1 {
-    text-align: center;
-  }
-  hr {
-    margin-left: auto;
-    margin-right: auto;
-  }
-  input {
-    border-radius: 0;
-    border: none;
-    background: transparent;
-    border-bottom: 1px solid rgba(black, 0.3);
-    &:focus {
-      border: none;
-      border-bottom: 1px solid rgba(black, 0.7);
-    }
-  }
-  button.filecoin {
-    margin: 0 auto;
-    margin-top: 2rem;
-    display: block;
-  }
-}
 .bottom-border {
-  margin-left: -2rem;
-  margin-right: -2rem;
+  margin-left: -50rem;
+  margin-right: -50rem;
   margin-bottom: -1rem;
   bottom: 0;
   height: 12px;
